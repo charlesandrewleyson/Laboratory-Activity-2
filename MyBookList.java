3cc2aed09a76f49f61eaa497a4e39439191b2c82
Charles Andrew Leyson <charlesandrewleyson25@gmail.com>
2025-10-05 23:27:50 +0800

    Update MyBookList.java

--- MyBookList.java
+++ MyBookList.java
@@ -1,17 +1,3 @@
-public class Book {
-    public String title;
-    public String author;
-    public int yearPublished;
-    public int currentPage;
-
-    public Book(String title, String author, int yearPublished, int currentPage) {
-        
-        this.title = title;
-        this.author = author;
-        this.yearPublished = yearPublished;
-        this.currentPage = currentPage;
-    }
-}
 public class MyBookList {
         // 1.) Create the following books that contains the title, author, year published. Use the Book class for this part.
         //      Rich Dad Poor Dad, Robert Kiyosaki, 1997
@@ -100,4 +86,5 @@ changePage();
     public static Book[] getBooks() {
         return Books;
     }
+
 }
